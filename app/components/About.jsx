d9c7fea6b2d1ed67f04adb6f0607cafcc58b3278
Ariken <ariken@youcanprevent.ca>
2017-11-01 10:25:52 -0400

    changed about component

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -6,8 +6,13 @@ var About=React.createClass({
 
         return(
 
+           <div>
+               <h3>About</h3>
+               <p>Welcome to the about page</p>
 
-            <h3>About Component</h3>
+           </div>
+
+
 
         );
     }
